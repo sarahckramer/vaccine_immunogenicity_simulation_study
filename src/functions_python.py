3997eebe54d83a60b60dcb4fda1a7c6b393d1a03
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-01-12 13:53:55 +0100

    Update to use non-mechanistic, biexponential model

--- src/functions_python.py
+++ src/functions_python.py
@@ -19,9 +19,7 @@ def generate_random_effects(median_val, sd_val, pop_size, logit=False):
         return out
     else:
         log_val = np.log(median_val)
-        # out = np.random.lognormal(mean=log_val, sigma=sd_perc_val*np.abs(log_val), size=pop_size)
         out = np.random.lognormal(mean=log_val, sigma=sd_val, size=pop_size)
-        # out = np.exp(np.random.normal(loc=log_val, scale=sd_val, size=pop_size))
         return out
 
 
@@ -35,15 +33,77 @@ def get_rate_from_half_life(d):
     return np.log(2) / d
 
 
-def calculate_Ab_titers(t_start, t_end, tau, A_m, beta, d_m, d_a, d_s, d_l, rho, N):
+def calculate_sigma_from_cv(desired_cv):
     """
-    Calculates synthetic antibody titers at each time point for each participant
+    Calculates standard deviation of a lognormal distribution corresponding to the input coefficient of variation
+
+    :param desired_cv: The intended coefficient of variation
+    :return: Standard deviation for the underlying normal distribution of a lognormal distribution
+    """
+    sigma = np.sqrt(np.log(desired_cv ** 2 + 1))
+    return sigma
+
+
+def calculate_Ab_titers_biexp(t_start, t_end, tau, alpha, beta, d_m, d_s, d_l, rho, N):
+    """
+    Calculates synthetic antibody titers at each time point for each participant according to a non-mechanistic
+    model accounting for biexponential antibody decay rates
+
+    :param t_start: First timepoint at which titers are calculated
+    :param t_end: Final timepoint at which titers are calculated (inclusive)
+    :param tau: Timepoints at which vaccine is given (1-D array)
+    :param alpha: Maternal antibody titers for each participant (1-D array)
+    :param beta: Individual-level values for beta at each vaccination timepoint (1 or 2-D array)
+    :param d_m: Half-life of maternal antibodies for each participant (1-D array)
+    :param d_s: Half-life of IgG antibodies for each participant (short-lived) (1-D array)
+    :param d_l: Half-life of IgG antibodies for each participant ("long-lived") (1-D array)
+    :param rho: Proportion of antibodies decaying at rate d_s (1-D array OR float64)
+    :param N: Size of the synthetic population
+    :return: Antibody titers for each time point (rows) and participant (columns)
+    """
+    # Start by calculating rates of decay from half-lives:
+    m = get_rate_from_half_life(d_m)
+    r1 = get_rate_from_half_life(d_s)
+    r2 = get_rate_from_half_life(d_l)
+
+    # Store Ab titers at each time point:
+    Ab_titers = np.zeros([len(range(t_start, t_end + 1)), N])
+
+    # Loop through each timepoint and calculate:
+    t = t_start
+    while t <= t_end:
+        maternal_ab = alpha * np.exp(-m * t)
+        A_t = maternal_ab
+
+        # add antibody response to each dose that has been given so far
+        if t >= min(tau):
+            for i in np.where(tau <= t)[0]:
+                tau_i = tau[i]
+
+                vaccine_ab = beta[i] * (rho * (np.exp(-r1 * (t - tau_i))) +
+                                        (1 - rho) * (np.exp(-r2 * (t - tau_i))))
+                A_t = A_t + vaccine_ab
+
+        # append titers at this timepoint to list
+        Ab_titers[t] = A_t
+
+        # move forward one day
+        t += 1
+
+    # Return titers over time:
+    return Ab_titers
+
+
+def calculate_Ab_titers_WhiteM3(t_start, t_end, tau, A_m, beta, d_m, d_a, d_s, d_l, rho, N):
+    """
+    Calculates synthetic antibody titers at each time point for each participant according to model #3
+    in White et al. (2014)
 
     :param t_start: First timepoint at which titers are calculated
     :param t_end: Final timepoint at which titers are calculated (inclusive)
     :param tau: Timepoints at which vaccine is given (1-D array)
     :param A_m: Maternal antibody titers for each participant (1-D array)
-    :param beta: Individual-level values for beta at each vaccination timepoint (2-D array)
+    :param beta: Individual-level values for beta at each vaccination timepoint (1- or 2-D array)
     :param d_m: Half-life of maternal antibodies for each participant (1-D array)
     :param d_a: Half-life of IgG antibodies for each participant (1-D array)
     :param d_s: Half-life of short-lived antibody secreting cells for each participant (1-D array)
@@ -62,7 +122,7 @@ def calculate_Ab_titers(t_start, t_end, tau, A_m, beta, d_m, d_a, d_s, d_l, rho,
     # Store Ab titers at each time point:
     Ab_titers = np.zeros([len(range(t_start, t_end + 1)), N])
 
-    # Loop through each time and calculate:
+    # Loop through each timepoint and calculate:
     t = t_start
     while t <= t_end:
         maternal_ab = A_m * np.exp(-m * t)
@@ -90,22 +150,30 @@ def calculate_Ab_titers(t_start, t_end, tau, A_m, beta, d_m, d_a, d_s, d_l, rho,
     return Ab_titers
 
 
-def add_random_noise(synth_titers):
+def add_random_noise(synth_titers, cv_natural):
     """
     Adds normally-distributed noise to "true" antibody titer values
 
     :param synth_titers: "True" (synthetic) antibody titers for all timepoints (rows) and individuals (columns)
+    :param cv_natural: Desired coefficient of variation on the natural scale
     :return: Noise-laden synthetic antibody titers for each timepoint (rows) and individual (columns)
     """
 
-    # Draw from normal distribution around "true" values:
-    noisy_titers = np.random.normal(loc=synth_titers, scale=0.1*synth_titers)
+    # Calculate desired sd:
+    sd = calculate_sigma_from_cv(cv_natural)
 
-    # Ensure that no values are <0:
-    neg_indices = np.where(noisy_titers < 0)
-    while len(neg_indices[0] > 0):
-        noisy_titers[neg_indices] = np.random.normal(loc=synth_titers[neg_indices], scale=0.1*synth_titers[neg_indices])
-        neg_indices = np.where(noisy_titers < 0)
+    # Draw from normal distribution around log of "true" values:
+    # noisy_titers_orig = np.random.normal(loc=synth_titers, scale=0.1*synth_titers)
+    noisy_titers = np.random.lognormal(mean=np.log(synth_titers), sigma=sd)
+    # print(np.std(np.reshape((noisy_titers - synth_titers) / synth_titers, [731000, ])))
+    # print(np.std(np.reshape((noisy_titers_orig - synth_titers) / synth_titers, [731000, ])))
+
+    # # Ensure that no values are <0:
+    # neg_indices = np.where(noisy_titers < 0)
+    # while len(neg_indices[0] > 0):
+    #     # noisy_titers[neg_indices] = np.random.normal(loc=synth_titers[neg_indices], scale=0.1*synth_titers[neg_indices])
+    #     noisy_titers[neg_indices] = np.random.lognormal(mean=np.log(synth_titers[neg_indices]), sigma=sd)
+    #     neg_indices = np.where(noisy_titers < 0)
 
     # Return noise-laden values:
     return noisy_titers
--- src/generate_synthetic_antibody_data.py
+++ src/generate_synthetic_antibody_data.py
@@ -1,4 +1,3 @@
-
 # Run model with random effects by participant #
 
 # Import necessary functions:
@@ -6,79 +5,86 @@ import pandas as pd
 from functions_python import *
 from functions_python_plot import *
 
-# def propagate_Ab_model(tmStrt, tmEnd, tmStep, A0, rho, beta, beta_s, beta_l, r, d_s, d_l, tau):
-#     dBs = rho * beta - c_s * B_s
-# but if the system can be solved... we can actually just plug all desired t into the one equation...
-# Although using the differential equations might make it easier to incorporate effects like age, season of birth, etc.?
-# Also could run using full ODEs and plot out not only Ab titers but also levels of ASCs
-
 #######################################################################################################################
 
 # Set global parameters:
 N_pop = 1000  # number of "participants"
+prop_short = np.float64(0.95)  # fit this as fixed for now
 
-# Set parameter means/sd:
+# Set parameter medians:
 maternal_antibodies_median = 0.2
-betas_median = np.array([0.035, 0.2, 1.0, 10.0])
+betas_median = np.array([10.0])  # single dose for now
+# betas_median = np.array([0.035, 0.2, 1.0, 10.0])
 half_life_maternal_median = 46.0  # days
-half_life_igg_median = 20.3  # days
-half_life_short_median = 5.5  # days
-half_life_long_median = 1648  # days
-prop_short_median = 0.98
-sd = 0.20
+half_life_short_median = 30.0  # days
+half_life_long_median = 365.0  # days
+
+# Get standard deviation from desired coefficient of variations (var/mean of distribution):
+sd = calculate_sigma_from_cv(0.1)
 
 # Set start, end, and vaccination timepoints:
 tm_start = 0
 tm_end = 730  # 2 years; tm_end = 1825  # 5 years
-vacc_timepoints = np.array([60, 120, 180, 365])  # vaccinate at 2, 4, 6 months, + booster at 12
+# vacc_timepoints = np.array([60, 120, 180, 365])  # vaccinate at 2, 4, 6 months, + booster at 12
+vacc_timepoints = np.array([365])  # vaccinate between 12 and 15 months; can be second dose at 4-6 yrs
 
 #######################################################################################################################
 
 # Set values for each participant:
 maternal_antibodies = generate_random_effects(maternal_antibodies_median, sd, N_pop)
 half_life_maternal = generate_random_effects(half_life_maternal_median, sd, N_pop)
-half_life_igg = generate_random_effects(half_life_igg_median, sd, N_pop)
 half_life_short = generate_random_effects(half_life_short_median, sd, N_pop)
 half_life_long = generate_random_effects(half_life_long_median, sd, N_pop)
 
-# To keep prop_short between 0-1, use logit-normal instead:
-prop_short = generate_random_effects(prop_short_median, sd, N_pop, True)
+# Check sd is correct:
+print(np.std(maternal_antibodies) / np.mean(maternal_antibodies))
+print(np.std(half_life_maternal) / np.mean(half_life_maternal))
+print(np.std(half_life_short) / np.mean(half_life_short))
+print(np.std(half_life_long) / np.mean(half_life_long))
+
+# # To keep prop_short between 0-1, use logit-normal instead:
+# prop_short = generate_random_effects(prop_short_median, sd, N_pop, True)
+# # to get distribution with same sd/cv, need to experiment - no analytic solution
+# N_pop = 1000000
+# a = generate_random_effects(prop_short, 1.1805, N_pop, True)
+# print(np.std(a) / np.mean(a))  # this depends on both the median and the sd
+# plt.hist(a, bins=100)  # but values may need to be tighter here, to keep values realistic
 
 # And get distribution for each value of beta, as well:
 betas = np.zeros([len(betas_median), N_pop])
 for i in range(len(betas_median)):
     betas[i] = generate_random_effects(betas_median[i], sd, N_pop)
-
+    print(np.std(betas[i]) / np.mean(betas[i]))  # check sd
 #######################################################################################################################
 
 # Run!
-sim_titers = calculate_Ab_titers(tm_start, tm_end, vacc_timepoints, maternal_antibodies, betas,
-                                 half_life_maternal, half_life_igg, half_life_short, half_life_long,
-                                 prop_short, N_pop)
+sim_titers = calculate_Ab_titers_biexp(tm_start, tm_end, vacc_timepoints, maternal_antibodies, betas,
+                                       half_life_maternal, half_life_short, half_life_long, prop_short,
+                                       N_pop)
 
-# Plot simulated "data":
-# plot_synth_ab_titers(sim_titers, save_path='results/PRELIM_plotTiters_20210104/',
+# # Plot simulated "data":
+# plot_synth_ab_titers(sim_titers, save_path='results/PRELIM_plotTiters_20210112/',
 #                      save_filename='ab_titers_over_time.png')
-# Note: For visualization purposes, plotted data were an earlier run with only 100 "participants;" 1000 were used to
-# generate the first round of synthetic data for fitting
+# # Note: For visualization purposes, plotted data were an earlier run with only 100 "participants;" 1000 were used to
+# # generate the first round of synthetic data for fitting
 
 # Write "true" values to file:
 if not os.path.isdir('data/'):
     os.mkdir('data/')
-if not os.path.isdir('data/prelim_check_20210104/'):
-    os.mkdir('data/prelim_check_20210104/')
+if not os.path.isdir('data/prelim_check_20210112/'):
+    os.mkdir('data/prelim_check_20210112/')
 
 true_vals = pd.DataFrame(sim_titers)
-true_vals.to_csv('data/prelim_check_20210104/truth.csv', na_rep='NA', index=False)
+true_vals.to_csv('data/prelim_check_20210112/truth.csv', na_rep='NA', index=False)
 del true_vals
 
 # Add random noise:
-sim_titers = add_random_noise(sim_titers)
+sim_titers = add_random_noise(sim_titers, 0.1)
 
-# Plot noise-laden "data":
-# plot_synth_ab_titers(sim_titers, save_path='results/PRELIM_plotTiters_20210104/',
+# # Plot noise-laden "data":
+# plot_synth_ab_titers(sim_titers, save_path='results/PRELIM_plotTiters_20210112/',
 #                      save_filename='ab_titers_over_time_NOISE.png')
 
 # Write synthetic data to file:
 obs_vals = pd.DataFrame(sim_titers)
-obs_vals.to_csv('data/prelim_check_20210104/obs_data.csv', na_rep='NA', index=False)
+obs_vals.to_csv('data/prelim_check_20210112/obs_data.csv', na_rep='NA', index=False)
