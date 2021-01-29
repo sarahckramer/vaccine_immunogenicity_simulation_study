d563a4ab5c924cc0e4a59e630998e31bfe44bff1
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-01-29 16:17:08 +0100

    Use nlsList as starting point for nlme when possible

--- src/fit_model_to_synth_data.R
+++ src/fit_model_to_synth_data.R
@@ -41,13 +41,13 @@ ab_titers$subject <- factor(ab_titers$subject)
 ab_titers <- ab_titers[ab_titers$time %in% select_timepoints[[sample_interval]], ]
 # need at least 2 points pre-vaccine, at least 4 post?
 
-# Plot data:
-p1 <- ggplot(data = ab_titers, aes(x = time, y = value, color = subject)) + geom_line() + geom_point() +
-  geom_vline(xintercept = 365, lty = 2) +
-  theme_classic() + theme(legend.position = 'None') + labs(x = 'Time (Days)', y = 'Ab Titers') +
-  scale_x_continuous(breaks = seq(0, max(ab_titers$time), by = 30)) +
-  scale_y_continuous(breaks = seq(0, max(ab_titers$value), by = 1.0))#, trans = 'log')
-print(p1)
+# # Plot data:
+# p1 <- ggplot(data = ab_titers, aes(x = time, y = value, color = subject)) + geom_line() + geom_point() +
+#   geom_vline(xintercept = 365, lty = 2) +
+#   theme_classic() + theme(legend.position = 'None') + labs(x = 'Time (Days)', y = 'Ab Titers') +
+#   scale_x_continuous(breaks = seq(0, max(ab_titers$time), by = 30)) +
+#   scale_y_continuous(breaks = seq(0, max(ab_titers$value), by = 1.0))#, trans = 'log')
+# print(p1)
 
 # Read in functions:
 source('src/functions_ab_titers_over_time.R')
@@ -58,7 +58,7 @@ m1 <- nls(log(value) ~ calculate_ab_titers_LOG(time, log_alpha, log_m, log_beta,
           data = ab_titers,
           start = c(log_alpha = log(5.0), log_m = log(log(2)/30), log_beta = log(18.0), logit_rho = logit(0.75),
                     log_r_1 = log(log(2)/30), log_r_2 = log(log(2)/3650)))
-plot(m1)
+summary(m1)
 
 # Next try nlsList (separate fits for each person):
 m2 <- nlsList(log(value) ~ calculate_ab_titers_LOG(time, log_alpha, log_m, log_beta, logit_rho, log_r_1, log_r_2) | subject,
@@ -78,41 +78,41 @@ m3 <- nlme(log(value) ~ calculate_ab_titers_LOG(time, log_alpha, log_m, log_beta
            start = c(log_alpha = log(5.0), log_m = log(log(2)/30), log_beta = log(18.0), logit_rho = logit(0.75),
                      log_r_1 = log(log(2)/30), log_r_2 = log(log(2)/3650)))
 m3.alt <- nlme(m2, random = pdDiag(log_alpha + log_m + log_beta + log_r_1 + log_r_2 ~ 1)) # fit is very similar
-plot(m3) # evidence of some increase in variance with values higher than about 1.5
-plot(m3, resid(.) ~ log(value), abline = 0)
-plot(m3, resid(.) ~ time, abline = 0)
-pairs(m3) # r_1 and r_2 might be correlated (moreso when more data points) - explore whether both random effects are needed
-qqnorm(m3, abline = c(0, 1)) # looks better when more data points are included
-qqnorm(m3, ~ ranef(.))
+plot(m3.alt) # evidence of some increase in variance with values higher than about 1.5
+# plot(m3, resid(.) ~ log(value), abline = 0)
+# plot(m3, resid(.) ~ time, abline = 0)
+pairs(m3.alt) # r_1 and r_2 might be correlated (moreso when more data points) - explore whether both random effects are needed
+qqnorm(m3.alt, abline = c(0, 1)) # looks better when more data points are included
+# qqnorm(m3, ~ ranef(.))
 # viz_model_fit(m3, ab_titers, logscale = TRUE)
 
 # Do we need random effect of r2?
-m4 <- update(m3, random = pdDiag(log_alpha + log_m + log_beta + log_r_1 ~ 1))
-anova(m3, m4)
+m4 <- update(m3.alt, random = pdDiag(log_alpha + log_m + log_beta + log_r_1 ~ 1))
+anova(m3.alt, m4)
 rm(m4)
 
-# Try to account for any correlation structure?:
-plot(ACF(m3, maxLag = 10), alpha = 0.05) # seems to be some autocorrelation?
-m4 <- update(m3, correlation = corAR1(abs(ACF(m3)[2, 2])))
-anova(m3, m4) # doesn't significantly improve fit (close for 250/2)
-# viz_model_fit(m4, ab_titers, logscale = TRUE)
-rm(m4)
-
-# What about variance functions?:
-m4 <- update(m3, weights = varPower(form = ~log(value)))
-anova(m3, m4)
-plot(m4, resid(.) ~ log(value), abline = 0)
-# seems to improve for some (more likely to make a difference when more timepoints/participants), but don't see much visual evidence
+# # Try to account for any correlation structure?:
+# plot(ACF(m3, maxLag = 10), alpha = 0.05) # seems to be some autocorrelation?
+# m4 <- update(m3, correlation = corAR1(abs(ACF(m3)[2, 2])))
+# anova(m3, m4) # doesn't significantly improve fit (close for 250/2)
+# # viz_model_fit(m4, ab_titers, logscale = TRUE)
+# rm(m4)
+# 
+# # What about variance functions?:
+# m4 <- update(m3, weights = varPower(form = ~log(value)))
+# anova(m3, m4)
+# plot(m4, resid(.) ~ log(value), abline = 0)
+# # seems to improve for some (more likely to make a difference when more timepoints/participants), but don't see much visual evidence
 
 # Now extract parameter fits and random effect sds:
-results.df <- get_param_est(m3)
+results.df <- get_param_est(m3.alt)
 print(results.df)
 
-# if (!dir.exists('results/PRELIM_nlme_res_20210118/')) {
-#   dir.create('results/PRELIM_nlme_res_20210118/')
+# if (!dir.exists('results/PRELIM_nlme_res_20210129/')) {
+#   dir.create('results/PRELIM_nlme_res_20210129/')
 # }
 # 
-# write.csv(results.df, file = paste0('results/PRELIM_nlme_res_20210118/res_n', n_participants, '_t', sample_interval, '.csv'), row.names = FALSE)
+# write.csv(results.df, file = paste0('results/PRELIM_nlme_res_20210129/res_n', n_participants, '_t', sample_interval, '.csv'), row.names = FALSE)
 
 # # saemix?:
 # ab_titers$log_value <- log(ab_titers$value)
