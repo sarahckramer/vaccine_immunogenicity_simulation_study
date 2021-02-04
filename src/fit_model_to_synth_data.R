a72c424514297bb3da85bd0811038bfb31e7fa44
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-02-04 08:46:23 +0100

    Remove fitting of maternal titers; clean up old fxns

--- src/fit_model_to_synth_data.R
+++ src/fit_model_to_synth_data.R
@@ -12,13 +12,22 @@ library(nlme)
 library(ggplot2)
 library(gridExtra)
 
+# Specify date (for results outputs):
+ymd <- '20210203'
+
 # Set number of data points/subjects:
 n_participants <- 50 # 50, 100, 250, 500, 1000
 sample_interval <- 1 # 1-4; selecting from list below
-select_timepoints <- list(c(seq(0, 360, by = 30), 379, seq(379+21, 1090, by = 30)),
-                          c(seq(0, 360, by = 60), 379, seq(379+51, 1090, by = 60)),
-                          c(0, 180, 360, 379, 550, 730, 1095),
-                          c(0, 360, 379, 730, 1095, 2190))
+select_timepoints <- list(c(379, seq(379+21, 1090, by = 30)),
+                          c(seq(379+51, 1090, by = 60)),
+                          c(379, 550, 730, 1095),
+                          c(379, 730, 1095, 2190))
+
+# select_timepoints <- list(c(365, 379, seq(379+21, 1090, by = 30)),
+#                           c(365, seq(379+51, 1090, by = 60)),
+#                           c(365, 379, 550, 730, 1095),
+#                           c(365, 379, 730, 1095, 2190))
+# # alternative to also include timepoint right before vacc; but here we're assuming negligible titers at this point, so might not need it
 
 # Set seed so that same subjects chosen each time:
 set.seed(3970395)
@@ -41,43 +50,46 @@ ab_titers$subject <- factor(ab_titers$subject)
 ab_titers <- ab_titers[ab_titers$time %in% select_timepoints[[sample_interval]], ]
 # need at least 2 points pre-vaccine, at least 4 post?
 
-# # Plot data:
-# p1 <- ggplot(data = ab_titers, aes(x = time, y = value, color = subject)) + geom_line() + geom_point() +
-#   geom_vline(xintercept = 365, lty = 2) +
-#   theme_classic() + theme(legend.position = 'None') + labs(x = 'Time (Days)', y = 'Ab Titers') +
-#   scale_x_continuous(breaks = seq(0, max(ab_titers$time), by = 30)) +
-#   scale_y_continuous(breaks = seq(0, max(ab_titers$value), by = 1.0))#, trans = 'log')
-# print(p1)
+# Set time of vaccine = time 0:
+ab_titers$time <- ab_titers$time - 365
+
+# Plot data:
+p1 <- ggplot(data = ab_titers, aes(x = time, y = value, color = subject)) + geom_line() + geom_point() +
+  # geom_vline(xintercept = 0, lty = 2) +
+  theme_classic() + theme(legend.position = 'None') + labs(x = 'Time (Days)', y = 'Ab Titers') +
+  scale_x_continuous(breaks = seq(0, max(ab_titers$time), by = 30)) +
+  scale_y_continuous(breaks = seq(0, max(ab_titers$value), by = 1.0))#, trans = 'log')
+print(p1)
 
 # Read in functions:
 source('src/functions_ab_titers_over_time.R')
 source('src/functions_assess_output.R')
 
 # First fit without random effects:
-m1 <- nls(log(value) ~ calculate_ab_titers_LOG(time, log_alpha, log_m, log_beta, logit_rho, log_r_1, log_r_2),
+m1 <- nls(log(value) ~ calculate_ab_titers_LOG_postOnly(time, log_beta, logit_rho, log_r_1, log_r_2),
           data = ab_titers,
-          start = c(log_alpha = log(5.0), log_m = log(log(2)/30), log_beta = log(18.0), logit_rho = qlogis(0.75),
-                    log_r_1 = log(log(2)/30), log_r_2 = log(log(2)/3650)))
+          start = c(log_beta = log(18.0), logit_rho = qlogis(0.75), log_r_1 = log(log(2)/30),
+                    log_r_2 = log(log(2)/3650)))
 summary(m1)
 
 # Next try nlsList (separate fits for each person):
-m2 <- nlsList(log(value) ~ calculate_ab_titers_LOG(time, log_alpha, log_m, log_beta, logit_rho, log_r_1, log_r_2) | subject,
+m2 <- nlsList(log(value) ~ calculate_ab_titers_LOG_postOnly(time, log_beta, logit_rho, log_r_1, log_r_2) | subject,
               data = ab_titers,
-              start = c(log_alpha = log(5.0), log_m = log(log(2)/30), log_beta = log(18.0), logit_rho = qlogis(0.75),
-                        log_r_1 = log(log(2)/30), log_r_2 = log(log(2)/3650)))
+              start = c(log_beta = log(18.0), logit_rho = qlogis(0.75), log_r_1 = log(log(2)/30),
+                        log_r_2 = log(log(2)/3650)))
 plot(intervals(m2)) # almost half not fit, but m seems most variable based on this plot; also alpha and beta
 # plot(m2, subject ~ resid(.), abline = 0 )
 pairs(m2)
 
 # Fit nlme:
-m3 <- nlme(log(value) ~ calculate_ab_titers_LOG(time, log_alpha, log_m, log_beta, logit_rho, log_r_1, log_r_2),
+m3 <- nlme(log(value) ~ calculate_ab_titers_LOG_postOnly(time, log_beta, logit_rho, log_r_1, log_r_2),
            data = ab_titers,
-           fixed = log_alpha + log_m + log_beta + logit_rho + log_r_1 + log_r_2 ~ 1,
-           random = pdDiag(log_alpha + log_m + log_beta + log_r_1 + log_r_2 ~ 1),
+           fixed = log_beta + logit_rho + log_r_1 + log_r_2 ~ 1,
+           random = pdDiag(log_beta + log_r_1 + log_r_2 ~ 1),
            groups = ~subject,
-           start = c(log_alpha = log(5.0), log_m = log(log(2)/30), log_beta = log(18.0), logit_rho = qlogis(0.75),
-                     log_r_1 = log(log(2)/30), log_r_2 = log(log(2)/3650)))
-m3.alt <- nlme(m2, random = pdDiag(log_alpha + log_m + log_beta + log_r_1 + log_r_2 ~ 1)) # fit is very similar
+           start = c(log_beta = log(18.0), logit_rho = qlogis(0.75), log_r_1 = log(log(2)/30),
+                     log_r_2 = log(log(2)/3650)))
+m3.alt <- nlme(m2, random = pdDiag(log_beta + log_r_1 + log_r_2 ~ 1)) # fit is very similar
 plot(m3.alt) # evidence of some increase in variance with values higher than about 1.5
 # plot(m3, resid(.) ~ log(value), abline = 0)
 # plot(m3, resid(.) ~ time, abline = 0)
@@ -87,7 +99,7 @@ qqnorm(m3.alt, abline = c(0, 1)) # looks better when more data points are includ
 # viz_model_fit(m3, ab_titers, logscale = TRUE)
 
 # Do we need random effect of r2?
-m4 <- update(m3.alt, random = pdDiag(log_alpha + log_m + log_beta + log_r_1 ~ 1))
+m4 <- update(m3.alt, random = pdDiag(log_beta + log_r_1 ~ 1))
 anova(m3.alt, m4)
 rm(m4)
 
@@ -108,11 +120,11 @@ rm(m4)
 results.df <- get_param_est(m3.alt)
 print(results.df)
 
-# if (!dir.exists('results/PRELIM_nlme_res_20210129/')) {
-#   dir.create('results/PRELIM_nlme_res_20210129/')
+# if (!dir.exists(paste0('results/PRELIM_nlme_res_', ymd, '/'))) {
+#   dir.create(paste0('results/PRELIM_nlme_res_', ymd, '/'))
 # }
 # 
-# write.csv(results.df, file = paste0('results/PRELIM_nlme_res_20210129/res_n', n_participants, '_t', sample_interval, '.csv'), row.names = FALSE)
+# write.csv(results.df, file = paste0('results/PRELIM_nlme_res_', ymd, '/res_n', n_participants, '_t', sample_interval, '.csv'), row.names = FALSE)
 
 # # saemix?:
 # ab_titers$log_value <- log(ab_titers$value)
